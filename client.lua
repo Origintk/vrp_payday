ca7b8cb0b47f3cc649860c8ca0fcbf68614cac1a
Sun <30409932+Origintk@users.noreply.github.com>
2019-01-22 09:33:18 -0200

    add tunnel , proxy

--- client.lua
+++ client.lua
@@ -1,9 +1,5 @@
- --[[
- SCRIPT CREATED ONLY FOR VRP !
- If u want change it for ESX, make a new server event and trigger it from client side.
- For any help, contact me! 
- www.krimes.ro | EskaPe#5716 (Discord) | discord.me/rpkrimes
---]]
+local Tunnel = module("vrp", "lib/Tunnel")
+local Proxy = module("vrp", "lib/Proxy")
 vRP = Proxy.getInterface("vRP")
 
 local minute = 30
